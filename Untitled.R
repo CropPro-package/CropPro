bffd9a75c08bdbafaa256871c243dc29f4513e42
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-02-18 14:52:36 +0000

    Set up

---
+++ Untitled.R
@@ -0,0 +1,36 @@
+
+LDAcrop.pro<-function(model, samples, ){
+
+}
+
+#x = lda1, y = lda2, z= lda3
+# note not sure how to do the colour - do have this as a default colour that can be changed.
+cropplot3dpoints<-function(x,y,z,col=c(???){
+  model<-READ IN LDA MODEL AND DATA
+  centroids<-READ OR CALCULATE centroids from above and see what they are called
+  open3d()
+  plot3d(model$lda1, model$lda2, model$lda3,
+       xlab="LD1", ylab="LD2", zlab="LD3", col=dataset$colour, type="s",size=0.8 )
+  shapelist3d(cube3d(),x=centroids$centroid1,y=centroids$centroid2, z=centroids$centroid3,  col="black",size=0.25)
+
+  points3d(x,y, z, col="black",  size=0.9)
+
+play3d( spin3d( axis = c(0, 0, 1), rpm = 20), duration = 10 )
+}
+
+wire3d(ellips1, col="blue")
+wire3d(ellips2, col="pink")
+wire3d(ellips3, col="purple")
+wire3d(ellips4, col="black")
+
+
+cropplot2d<-function(x???,y???,xlims=c(-5,5),ylims=c(-5.5,4),cols=,pchs,legend){
+  LDA.data<-
+  centroids<-
+  par(mar=c(10,4,4,4))
+  plot(x=dataset$lda.LD1, y=dataset$lda.LD2, col=dataset$colour, pch=as.numeric(as.character(dataset$pch)), ylim=c(-5.5,4), xlim=c(-5,5), xlab="", ylab="")
+  par(new=T)
+  plot(centroids$centroid1,centroids$centroid2 , col="Black", pch=20, ylim=c(-5.5,4), xlim=c(-5,5), xlab="Function 1", ylab="Function 2")
+  par(new=T)
+  plot(predictions$x.LD1,predictions$x.LD2, col="Black", pch=17,ylim=c(-5.5,4), xlim=c(-5,5), xlab="Function 1", ylab="Function 2")
+}
