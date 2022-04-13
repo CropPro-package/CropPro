89b656ef94ea9731def25c5a5725ccd48643ec46
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-04-13 17:15:20 +0100

    new data organisation R script

--- R/dataorg.R
+++ R/dataorg.R
@@ -1,6 +1,6 @@
 
 library(plyr)
-
+library(data.table)
 rowlab<-Test[,1:6]
 Ncol<-ncol(Test)
 test2<-Test[,7:ncol(test)]
@@ -9,24 +9,26 @@ total<-numcolwise(sum)(test2)
 
 all<-test2
 
-Due to labels at fround start at 7
+# Due to labels at found start at 7
+TEST<-Test[,6:ncol(Test)]
 
 
-This works without the labels
+#This works without the labels
 
 dataorg<-function(dataframe, labels){
-
-  REMOVE LABELS
-  apply2<-apply(dataframe,2, function(x){sqrt(x*100/sum(x))})
-  Add labels back
-  Sum by group
-  new columns labels BHH, BFH, SHH, SHL, SFH, SFL
+data<-dataframe[,(labels+1):ncol(dataframe)]
+removed<-dataframe[,1:labels]
+apply2<-apply(data,2, function(x){sqrt(x*100/sum(x))})
+all<-cbind(removed,apply2)
+DT <- as.data.table(all)
+# which columns are numeric
+numeric_cols <- which(sapply(DT, is.numeric))
+Y<-DT[, lapply(.SD, sum), by = `2`, .SDcols = numeric_cols]
+  #new columns labels BHH, BFH, SHH, SHL, SFH, SFL
+n<-Y$`2`
+df<-as.data.frame(t(Y[,-1]))
+colnames(df)<-n
+colorder<-c("BHH", "BFH","SHH","SHL", "SFH", "SFL")
+dforder<-df[,colorder]
 }
-
-dataorg<-function(dataframe, labels){
-  for(i in labels:ncol(all)){
-    apply<-apply(all,2, function(x){sqrt(x*100/sum(x))})
-  }
-
-
-new<-dataorg(dataframe=Test)
+summaryGroup <- ddply(big.data, c("Site", "FSphase", "English"), function(x) c(d15N=mean(x$adjustedN15), sd=sd(x$adjustedN15),  d13C=mean(x$adjustedC13), sd=sd(x$adjustedC13)))
