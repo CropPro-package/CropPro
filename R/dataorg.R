6e4b1afc22280c3bafc2572666a004429b24ddc7
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-04-21 09:54:26 +0100

    data organisation script and documentation

--- R/dataorg.R
+++ R/dataorg.R
@@ -1,34 +1,14 @@
-
 library(plyr)
-library(data.table)
-rowlab<-Test[,1:6]
-Ncol<-ncol(Test)
-test2<-Test[,7:ncol(test)]
-Nrow<-nrow(all)
-total<-numcolwise(sum)(test2)
-
-all<-test2
-
-# Due to labels at found start at 7
-TEST<-Test[,6:ncol(Test)]
-
-
-#This works without the labels
-
-dataorg<-function(dataframe, labels){
-data<-dataframe[,(labels+1):ncol(dataframe)]
-removed<-dataframe[,1:labels]
-apply2<-apply(data,2, function(x){sqrt(x*100/sum(x))})
-all<-cbind(removed,apply2)
-DT <- as.data.table(all)
-# which columns are numeric
-numeric_cols <- which(sapply(DT, is.numeric))
-Y<-DT[, lapply(.SD, sum), by = `2`, .SDcols = numeric_cols]
-  #new columns labels BHH, BFH, SHH, SHL, SFH, SFL
-n<-Y$`2`
-df<-as.data.frame(t(Y[,-1]))
-colnames(df)<-n
+dataorg<-function(dataframe, groupcol){
+data<-dataframe[,(groupcol+1):ncol(dataframe)]
+groups<-dataframe[,groupcol]
+colnames(groups)<-"category"
+apply2<-apply(data,2,function(x){sqrt(x*100/sum(x))})
+all<-cbind(groups,apply2)
+sumgroups<-aggregate(.~`category`, data=all, FUN=sum)
+labels<-toupper(sumgroups$`category`)
+df<-as.data.frame(t(sumgroups[,-1]))
+colnames(df)<-labels
 colorder<-c("BHH", "BFH","SHH","SHL", "SFH", "SFL")
 dforder<-df[,colorder]
 }
-summaryGroup <- ddply(big.data, c("Site", "FSphase", "English"), function(x) c(d15N=mean(x$adjustedN15), sd=sd(x$adjustedN15),  d13C=mean(x$adjustedC13), sd=sd(x$adjustedC13)))
