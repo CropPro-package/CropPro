acbd49ff9d2d07179d1bc014128fd94c18de4682
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-08-10 17:17:15 +0100

    New dung based function

---
+++ R/LDAcrop.dung.R
@@ -0,0 +1,27 @@
+LDAcrop.dung<-function(x){
+data.model<-data.frame(data.model)
+model.arch<-rbind(data.model, x)
+discrim_cv <- lda(PROC ~ BHH+BFH+SHH+SHL+SFH+SFL, model.arch, CV = TRUE)
+model_lda <- lda(PROC ~ BHH+BFH+SHH+SHL+SFH+SFL, model.arch)
+predictionmodel <- predict(model_lda, model.arch)
+functionalAt <- data.frame(PROC = as.factor(model.arch$PROC),
+                           Classification= predictionmodel$class,
+                           predictionmodel$x)
+centroids <- functionalAt %>%
+  group_by(PROC) %>%
+  summarise(centroid1 = mean(LD1),
+            centroid2= mean(LD2),
+            centroid3= mean(LD3),
+            centroid4 = mean(LD4))
+models <- cbind(as.data.frame(predict(model_lda,x)),x)
+tbl <- table(models$class)
+res <- cbind(tbl,round(prop.table(tbl)*100,2))
+colnames(res) <- c('Count','Percentage')
+row.names(res)<-c("Archaeological", "Winnowing by-products", "Coarse-sieving by-products", "Fine-sieving by-products", "Fine-sieving products")
+print("results and linear discriminant scores")
+print(models)
+print("classifications")
+print(res)
+models<-models
+
+}
--- R/LDAcrop.pro.R
+++ R/LDAcrop.pro.R
@@ -1,20 +1,31 @@
-
 LDAcrop.pro<-function(x){
 
   data.model<-data.frame(data.model)
+
   discrim_cv <- lda(PROC ~ BHH+BFH+SHH+SHL+SFH+SFL,data.model, CV = TRUE)
+
   model_lda <- lda(PROC ~ BHH+BFH+SHH+SHL+SFH+SFL,data.model)
+
   predictionmodel <- predict(model_lda,data.model)
+
   functionalAt <- data.frame(PROC = as.factor(data.model$PROC),
                              Classification= predictionmodel$class,
                              predictionmodel$x)
+
   centroids <- functionalAt %>%
     group_by(PROC) %>%
     summarise(centroid1 = mean(LD1),
               centroid2= mean(LD2),
               centroid3=mean(LD3))
+
   model <- cbind(as.data.frame(predict(model_lda,x)),x)
-
+  tbl <- table(model$class)
+  res <- cbind(tbl,round(prop.table(tbl)*100,2))
+  colnames(res) <- c('Count','Percentage')
+  row.names(res)<-c("Winnowing by-products", "Coarse-sieving by-products", "Fine-sieving by-products", "Fine-sieving products")
+  print("results and linear discriminant scores")
   print(model)
-
+  print("classifications")
+  print(res)
+results<-model
 }
