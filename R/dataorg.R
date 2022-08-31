77d6b27f4632453b9243be7b2acdd0949cdd7553
Elizabeth Stroud <elizabethastroud@icloud.com>
2022-08-31 17:12:00 +0100

    editing of man docs, fixing dataorg

--- R/dataorg.R
+++ R/dataorg.R
@@ -2,13 +2,22 @@ library(plyr)
 dataorg<-function(dataframe, groupcol){
 data<-dataframe[,(groupcol+1):ncol(dataframe)]
 groups<-dataframe[,groupcol]
+groups<-as.data.frame(groups)
 colnames(groups)<-"category"
+
 apply2<-apply(data,2,function(x){sqrt(x*100/sum(x))})
 all<-cbind(groups,apply2)
 sumgroups<-aggregate(.~`category`, data=all, FUN=sum)
 labels<-toupper(sumgroups$`category`)
 df<-as.data.frame(t(sumgroups[,-1]))
 colnames(df)<-labels
-colorder<-c("BHH", "BFH","SHH","SHL", "SFH", "SFL")
+resultdf<-data.frame #OF 7 columns and X row number of samples ( so number of columns data has)
+resultdf$samples<-rownames(df)
+resultdf$BHH<-df$BHH
+resultdf$BFH<-df$BFH
+resultdf$SHH<-df$SHH
+resultdf$SHL<-df$SHL
+resultdf$SFH<-df$SFH
+
 dforder<-df[,colorder]
 }
